2308d2d37d4e2a42b6e67cab05465c47354b5ad5
Lisbeth <13511707@qq.com>
2021-08-27 01:16:09 +0800

    first commmit

---
+++ src/pages/home.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './home.scss';
+
+export const Home = () => {
+  return (
+    <div className="home">
+      <header>
+        <span>北京市实时医疗数据统计</span>
+        <div className="halo"></div>
+      </header>
+      <main>
+        <section className="section1"></section>
+        <section className="section2"></section>
+        <section className="section3"></section>
+        <section className="section4"></section>
+        <section className="section5"></section>
+        <section className="section6"></section>
+      </main>
+    </div>
+  );
+};
